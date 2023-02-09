dcf479146f16bed9658b286f03691de0b1ad7303
Sadman Sakib <sadmansakib123450@gmail.com>
2023-02-09 20:49:07 +0100

    another code addre named beerFunction

---
+++ beerFunction.js
@@ -0,0 +1,22 @@
+
+function beer(){
+    var bottle = 99;
+     
+    while(bottle>=1){
+        console.log(bottle+" bottle of beer on the waal "+ bottle +" bottles of beer");
+        bottle= bottle-1;
+        console.log("Take one down pass it around " +bottle+ ", bottles of beer on the wall");
+   
+     if(bottle === 1){
+    
+        
+   
+  }
+    console.log("No more bottles of beer on the wall");
+    }
+   
+  
+    
+}
+ 
+beer();
